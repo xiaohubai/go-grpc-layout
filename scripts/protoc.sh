a3cb3ec179146ffaa4ba7d41b80cb3c27ad77536
xiaohubai <xiaohubai@outlook.com>
2024-05-22 23:49:20 +0800

    v0.1

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -3,7 +3,6 @@ mkdir temp && cd temp
 wget https://github.com/protocolbuffers/protobuf/releases/download/v26.0/protoc-26.0-linux-x86_64.zip
 unzip protoc-26.0-linux-x86_64.zip
 
-rm -rf $GOPATH/bin/protoc
 mv bin/protoc $GOPATH/bin
 cd ../
 rm -rf temp
